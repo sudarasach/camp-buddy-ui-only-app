c50efafcd60b4f53c23825d39da3a8ba8356e7ff
sudarasach <sudara@orelit.com>
2025-10-25 10:00:26 +0530

    settings screen completed

--- lib/modules/settings_screen.dart
+++ lib/modules/settings_screen.dart
@@ -108,31 +108,29 @@ class SettingsScreen extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                 ),
               ),
-              
-              SizedBox(height: 20),
 
-              Container(),
+              SizedBox(height: 40),
 
-              
+              _buildSettingsCard(
+                  Icons.person_outline_outlined,
+                  "Personal Details",
+                  context),
 
-              // Container(
-              //   padding: const EdgeInsets.all(20.0),
-              //   child: ListView.builder(
-              //     itemCount: chatDataList.length,
-              //     shrinkWrap: true,
-              //     // physics: const NeverScrollableScrollPhysics(),
-              //     itemBuilder: (context, index) {
-              //       final item = chatDataList[index];
-              //       return _buildChatCard(
-              //         item["img"]!,
-              //         item["header"]!,
-              //         item["title"]!,
-              //         item["sub"]!,
-              //         context, // pass context properly
-              //       );
-              //     },
-              //   ),
-              // ),
+                  _buildSettingsCard(
+                  Icons.question_mark_outlined,
+                  "Help & Support",
+                  context),
+
+                  _buildSettingsCard(
+                  Icons.privacy_tip_outlined,
+                  "Privacy & Policy",
+                  context),
+
+                   _buildSettingsCard(
+                  Icons.content_paste_search_outlined,
+                  "Terms & Conditions",
+                  context),
+
             ],
           ),
         ],
@@ -141,140 +139,46 @@ class SettingsScreen extends StatelessWidget {
   }
 }
 
-Widget _buildChatCard(
-  String imageUrl,
-  String header,
-  String title,
-  String subTitle,
-  BuildContext context,
-) {
+Widget _buildSettingsCard(IconData icon, String title, BuildContext context) {
   return Container(
-    margin: EdgeInsets.only(bottom: 25),
-    padding: EdgeInsets.all(15),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(15),
-      color: Colors.white,
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.08), // light shadow
-          blurRadius: 15, // smooth blur
-          spreadRadius: 5, // subtle spread
-          offset: Offset(0, 2), // vertical shadow like in image
-        ),
-      ],
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        //* header text
-        Text(
-          header,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-
-        SizedBox(height: 15),
-
+        //!icon
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            //* image & titles
-            Row(
-              children: [
-                //! profile image
-                ClipOval(
-                  child: Image.asset(
-                    imageUrl,
-                    height: 40.0,
-                    width: 40.0,
-                    fit: BoxFit.cover, //change image fill type
-                  ),
-                ),
-
-                SizedBox(width: 10),
-
-                //* titles
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //! title
-                    Text(
-                      title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-
-                    SizedBox(height: 2),
-
-                    //!subtitle
-                    Text(
-                      subTitle,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            Container(
+              padding: EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 143, 190, 146),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: 
+              Icon(
+               icon,
+                size: 23,
+                color: const Color.fromARGB(255, 21, 100, 44),
+              ),
             ),
-
-            //* action icon buttons
-            Row(
-              children: [
-                //! message icon
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.08), // light shadow
-                        blurRadius: 15, // smooth blur
-                        spreadRadius: 5, // subtle spread
-                        offset: Offset(0, 2), // vertical shadow like in image
-                      ),
-                    ],
-                  ),
-                  child: Icon(
-                    Icons.message,
-                    size: 20,
-                    color: HexColor("#678032"),
-                  ),
-                ),
-
-                SizedBox(width: 10),
-
-                //! call icon
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.08), // light shadow
-                        blurRadius: 15, // smooth blur
-                        spreadRadius: 5, // subtle spread
-                        offset: Offset(0, 2), // vertical shadow like in image
-                      ),
-                    ],
-                  ),
-                  child: Icon(Icons.call, size: 20, color: Colors.deepOrange),
-                ),
-              ],
+            SizedBox(width: 20),
+            //!subtitle
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
             ),
           ],
+        ),
+
+        //!arrow icon
+        Icon(
+          Icons.arrow_forward_ios_outlined,
+          size: 22,
+          color: const Color.fromARGB(255, 21, 100, 44),
         ),
       ],
     ),
